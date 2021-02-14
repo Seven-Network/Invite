b5b266eb5e72dd80d917c790b446b1584c81c906
Sameer Niaz Mohammad <niazmohsameer@gmail.com>
2021-02-14 14:50:20 +0800

    Add room broadcasting

--- src/lobby.js
+++ src/lobby.js
@@ -1,3 +1,6 @@
+const { v4: uuidv4 } = require('uuid');
+const messagePack = require('messagepack');
+
 class Lobby {
   constructor(roomID) {
     this.roomID = roomID;
@@ -5,15 +8,43 @@ class Lobby {
   }
 
   addUser(user) {
+    user.lobby = this;
     this.users.push(user);
     console.log(`${user.playerName} joined ${this.roomID}`);
+    this.broadcastRoom();
+  }
+
+  removeUser(id) {
+    const index = this.users.findIndex((val) => {
+      if (val.id === id) return true;
+      else return false;
+    });
+    this.users.splice(index, 1);
+    this.broadcastRoom();
+  }
+
+  broadcastRoom() {
+    var users = [];
+    for (var i = 0; i < this.users.length; i++) {
+      users.push(this.users[i].playerName);
+    }
+    const data = messagePack.encode(["room", users, true, true, false]);
+    for (var i = 0; i < this.users.length; i++) {
+      this.users[i].ws.send(data);
+    }
   }
 }
 
 class LobbyUser {
   constructor(playerName, ws) {
+    this.id = uuidv4();
     this.playerName = playerName;
     this.ws = ws;
+    this.lobby = null;
+
+    ws.on('close', () => {
+      this.lobby.removeUser(this.id);
+    });
   }
 }
 
